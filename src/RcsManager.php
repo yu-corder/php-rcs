231913677892f9a892fd5433dd4710ec963945e2
yu-corder <yuuya.9602@icloud.com>
2026-05-20 16:51:36 +0900

    Add: Implement checkin method in RcsManager

--- src/RcsManager.php
+++ src/RcsManager.php
@@ -21,4 +21,34 @@ class RcsManager
 
         return true;
     }
+
+    /**
+     * @param $string, $string
+     */
+    public function checkin(string $filePath, string $commet): bool {
+
+        if (is_dir($filePath)) {
+            return false;
+        }
+
+        $dir = dirname($filePath);
+        $file = basename($filePath);
+        $rcsPath = $dir . '/.rcs/';
+
+        $files = glob($rcsPath . $file . '*');
+        $rev = ".v";
+        $version = 1;
+        foreach($files as $revfile) {
+            $tmpfile = basename($revfile);
+            $v = strrpos($tmpfile, 'v');
+            $tmp = substr($tmpfile, $v + 1) + 1;
+            if ($version <= $tmp) $version = $tmp;
+        }
+
+        $final_rev = $rev . $version;
+        if (!copy($filePath, $rcsPath . $file . $final_rev)) {
+            return false;
+        }
+        return true;
+    }
 }
