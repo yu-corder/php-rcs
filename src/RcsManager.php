6af444424648b48566d866d32569f0d594f00681
yu-corder <yuuya.9602@icloud.com>
2026-05-21 10:18:29 +0900

    Remove: Delete obsolete init method since checkin handles directory creation automatically

--- src/RcsManager.php
+++ src/RcsManager.php
@@ -7,22 +7,6 @@ namespace YuCorder;
 class RcsManager
 {
     /**
-     * @param $string
-     */
-    public function init (string $projectPath): bool {
-        $rcsPath = rtrim($projectPath, '/') . '/.rcs';
-
-        if (is_dir($rcsPath)) {
-            return true;
-        }
-        if (!mkdir($rcsPath, 0755, true)) {
-            return false;
-        }
-
-        return true;
-    }
-
-    /**
      * @param $string, $string
      */
     public function checkin(string $filePath, string $comment): bool {
