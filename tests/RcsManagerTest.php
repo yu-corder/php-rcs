c649cc324c508669288889522ed0ceba50a62efc
yu-corder <yuuya.9602@icloud.com>
2026-05-22 17:04:16 +0900

    Test: Add exception tests for checkin, log, and checkout edge cases

--- tests/RcsManagerTest.php
+++ tests/RcsManagerTest.php
@@ -13,6 +13,8 @@ class RcsManagerTest extends TestCase
     private string $testFile;
     private string $rcsJson;
     private string $rcsDir;
+    private string $errorFile;
+    private string $errorDir;
 
     protected function setUp(): void
     {
@@ -20,6 +22,8 @@ class RcsManagerTest extends TestCase
         $this->testFile = __DIR__ . '/sandbox.txt';
         $this->rcsDir = __DIR__ . '/.rcs';
         $this->rcsJson = $this->rcsDir . '/sandbox.txt.json';
+        $this->errorFile = __DIR__ . '/error.txt';
+        $this->errorDir = __DIR__ . '/';
 
         $this->cleanUpFiles();
     }
@@ -77,6 +81,52 @@ class RcsManagerTest extends TestCase
         $file_content = file_get_contents($this->testFile);
         $this->assertSame("Hello, World! - Version 1", $file_content);
 
+    }
+
+    public function test_check_in_throws_exception_when_file_does_not_exist(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->rcs->checkIn($this->errorFile, "First commit!");
+    }
+
+    public function test_log_throws_exception_when_file_does_not_exist(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->rcs->log($this->errorFile);
+    }
+
+    public function test_log_throws_exception_when_path_is_directory(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->rcs->log($this->errorDir);
+    }
+
+    public function test_log_throws_exception_when_non_existent_history(): void
+    {
+        file_put_contents($this->testFile, "Hello");
+        $this->rcs->checkIn($this->testFile, "Init");
+        $this->expectException(\InvalidArgumentException::class);
+        $history = $this->rcs->log($this->testFile, 100);
+    }
+
+    public function test_check_out_throws_exception_when_file_does_not_exist(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        $this->rcs->checkOut($this->errorFile, 1);
+    }
+
+    public function test_check_out_throws_exception_when_path_is_directory(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->rcs->checkOut($this->errorDir, 1);
+    }
+
+    public function test_check_out_throws_exception_when_non_existent_version(): void
+    {
+        file_put_contents($this->testFile, "Hello");
+        $this->rcs->checkIn($this->testFile, "Init");
+        $this->expectException(\InvalidArgumentException::class);
+        $this->rcs->checkOut($this->testFile, 100);
     }
 
 }
